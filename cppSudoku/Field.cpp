e9cdec58b174e394bdd2148a169f155973172071
Alexander <boshurik666@mail.ru>
2012-02-18 21:57:35 +0400

    Доделал класс Filed

--- cppSudoku/Field.cpp
+++ cppSudoku/Field.cpp
@@ -4,9 +4,31 @@
 
 Field::Field(void)
 {
+	this->number = 0;
+	this->changeable = true;
 }
 
+Field::Field(int number, bool changeable)
+{
+	this->number = number;
+	this->changeable = changeable;
+}
 
 Field::~Field(void)
 {
+}
+
+void Field::setNumber(int number)
+{
+	this->number = number;
+}
+
+int Field::getNumber()
+{
+	return this->number;
+}
+
+bool Field::isChangeable()
+{
+	return this->changeable;
 }

584bd7babfe633a3498758491df667612b46ed4f
Alexander <boshurik666@mail.ru>
2012-02-21 23:37:24 +0400

    Инициализация через коммандную строку

--- cppSudoku/cppSudoku.cpp
+++ cppSudoku/cppSudoku.cpp
@@ -20,7 +20,37 @@ void printSudoku(Sudoku sudoku)
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	if (argc != 2)
+	{
+		printf("Not enough parameters\n");
+		return 0;
+	}
+
+
+	_TCHAR *initString = NULL;
+	initString = (_TCHAR *)calloc(9 * 9 + 1, sizeof(_TCHAR));
+	memcpy(initString, argv[1], 9 * 9 * sizeof(_TCHAR));
+
+	int initStringLenght = _tcslen(initString);
+	if (initStringLenght != (9 * 9))
+	{
+		printf("Lenght of the init string must be 81\n");
+		return 0;
+	}
+	
+	_TCHAR initChar[2] = _T("");
 	int matrix[9][9];
+	int x, y;
+	
+	for (int i = 0; i < initStringLenght; i++)
+	{
+		x = i % 9;
+		y = i / 9;
+
+		_stprintf(initChar, _T("%c"), initString[i]);
+
+		matrix[x][y] = _ttoi(initChar);
+	}
 
 	LARGE_INTEGER li;
     QueryPerformanceFrequency(&li);
@@ -29,42 +59,6 @@ int _tmain(int argc, _TCHAR* argv[])
 
     QueryPerformanceCounter(&li);
     __int64 CounterStart = li.QuadPart;
-
-	matrix[0][0] = 2; matrix[1][0] = 0; matrix[2][0] = 0; 
-	matrix[3][0] = 0; matrix[4][0] = 0; matrix[5][0] = 0; 
-	matrix[6][0] = 0; matrix[7][0] = 0; matrix[8][0] = 8; 
-
-	matrix[0][1] = 0; matrix[1][1] = 0; matrix[2][1] = 0; 
-	matrix[3][1] = 0; matrix[4][1] = 0; matrix[5][1] = 9; 
-	matrix[6][1] = 3; matrix[7][1] = 0; matrix[8][1] = 0; 
-
-	matrix[0][2] = 8; matrix[1][2] = 0; matrix[2][2] = 5; 
-	matrix[3][2] = 3; matrix[4][2] = 6; matrix[5][2] = 0; 
-	matrix[6][2] = 0; matrix[7][2] = 4; matrix[8][2] = 0; 
-
-	matrix[0][3] = 0; matrix[1][3] = 3; matrix[2][3] = 0; 
-	matrix[3][3] = 0; matrix[4][3] = 8; matrix[5][3] = 0; 
-	matrix[6][3] = 5; matrix[7][3] = 0; matrix[8][3] = 0; 
-
-	matrix[0][4] = 7; matrix[1][4] = 0; matrix[2][4] = 0; 
-	matrix[3][4] = 0; matrix[4][4] = 0; matrix[5][4] = 0; 
-	matrix[6][4] = 0; matrix[7][4] = 0; matrix[8][4] = 4; 
-
-	matrix[0][5] = 0; matrix[1][5] = 0; matrix[2][5] = 4; 
-	matrix[3][5] = 0; matrix[4][5] = 1; matrix[5][5] = 0; 
-	matrix[6][5] = 0; matrix[7][5] = 7; matrix[8][5] = 0; 
-
-	matrix[0][6] = 0; matrix[1][6] = 4; matrix[2][6] = 0; 
-	matrix[3][6] = 0; matrix[4][6] = 5; matrix[5][6] = 7; 
-	matrix[6][6] = 8; matrix[7][6] = 0; matrix[8][6] = 6; 
-
-	matrix[0][7] = 0; matrix[1][7] = 0; matrix[2][7] = 1; 
-	matrix[3][7] = 2; matrix[4][7] = 0; matrix[5][7] = 0; 
-	matrix[6][7] = 0; matrix[7][7] = 0; matrix[8][7] = 0; 
-
-	matrix[0][8] = 9; matrix[1][8] = 0; matrix[2][8] = 0; 
-	matrix[3][8] = 0; matrix[4][8] = 0; matrix[5][8] = 0; 
-	matrix[6][8] = 0; matrix[7][8] = 0; matrix[8][8] = 2; 
 
 	Sudoku sudoku(matrix);
 
